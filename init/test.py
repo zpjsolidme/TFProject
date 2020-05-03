1552988521f8eabd127764a7d5e261a5ed6c5f61
zpj <550392179@qq.com>
2020-05-03 20:29:12 +0800

    Test

---
+++ init/test.py
@@ -0,0 +1,9 @@
+'''
+@Author: your name
+@Date: 2020-05-03 20:28:29
+@LastEditTime: 2020-05-03 20:28:58
+@LastEditors: Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: \TfTest\init\test.py
+'''
+print('hello')
